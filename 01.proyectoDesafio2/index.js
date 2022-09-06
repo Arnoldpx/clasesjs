7da5db3cc18c75e8b8310b4b8a5f9fb9b9810bc9
Arnaldo sotomayor <sotomayor85@hotmail.com>
2022-09-06 15:04:39 -0300

    modificacion del array

--- 01.proyectoDesafio2/index.js
+++ 01.proyectoDesafio2/index.js
@@ -1,28 +1,29 @@
-function Clientes(nombre ,dni, sueldo){
-    this.nombre = nombre;
-    this.dni = dni;
-    this.sueldo = sueldo;
-}
-   let nombre = prompt("ingrese nombre:");
-   let dni = prompt("ingrese Dni:");
-   let sueldo  =parseInt(prompt("ingrese el monto de su sueldo:"))
-if(nombre && dni && sueldo){
-   let usuario = new Clientes(nombre , dni , sueldo)
-   console.log(usuario)
-}else
-{
-   console.log("Faltan Datos")
-}
-const personas = []
 
-    personas.push(Clientes());
+class Clientes{
+    constructor(nombre,monto,plazo,valorCuotas, total){
+        this.nombre = nombre;
+        this.monto = monto;
+        this.plazo = plazo;
+        this.valorCuotas = valorCuotas;
+        this.total = total; 
+    }
+     
+}
+const personas = [];
 
-function cotizador(Clientes, monto, plazo){
-    valor_cuotas = monto / plazo;
+cotizador (
+    prompt('Ingrese su nombra'),
+    parseInt(prompt('ingrese el monto solicitado')),
+    parseInt(prompt('Ingrese la cantidad de coutas, 12 . 24 . 48 .72'))
+    )
+
+function cotizador(nombre,monto,plazo){ 
+
+    let valorCuotas = monto / plazo;
     let interes;
+    let total = valorCuotas + interes;
+
     const tasa = [0.60 , 0.80 , 0.100 , 0.120]
-    
-console.log(personas);
     if(plazo==12){
         interes  =monto * tasa[0] / plazo
     }else if(plazo==24){
@@ -34,25 +35,28 @@ console.log(personas);
     }else{
         console.log("el plazo no es correcto")
     }
-    console.log('datos del clientes', Clientes);
+    total=valorCuotas + interes;
     console.log('monto a solicitar es',monto);
     console.log('La cantidad de coutas en devolver es de', plazo);
-    console.log('su cuota pura es', valor_cuotas.toFixed() );
-    console.log('el total de intereses es ' , valor_cuotas + interes.toFixed());
-    let validacion= prompt('Desea realizar la operacion? SI/NO');
+    console.log('su cuota pura es', valorCuotas );
+    console.log('el total de intereses es ' , valorCuotas + interes);
+      newPersona = new Clientes(nombre,monto,plazo,valorCuotas,total )  
+    console.log(newPersona)
+     
+        let validacion= prompt('Desea realizar la operacion? SI/NO');
 
     if(validacion.toUpperCase() == 'SI' || validacion.toUpperCase() == 'NO'){
         alert('su operacion se realizo con exito.')
+        
     }else{
         alert('muchas gracias Felicitaciones')
-    }
+    }   
+    agrega();
 }
-cotizador (
-    parseInt(prompt('ingrese el monto solicitado')),
-    parseInt(prompt('Ingrese la cantidad de coutas, 12 . 24 . 48 .72'))
-
+agrega()
+   function agrega(){
+   personas.push(newPersona);
+   console.log(personas);
+   }
     
-)
-
-
     
